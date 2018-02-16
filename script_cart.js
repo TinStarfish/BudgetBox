54138bb02e24eb2a1a53353a23a2b4d3e180d28a
TinStarfish <Waterstrevor68@yahoo.com>
2018-02-15 19:11:02 -0700

    More Checkout
    Added customer payment methods and working cart

--- script_cart.js
+++ script_cart.js
@@ -2,29 +2,67 @@ console.log("Cart script activating...");
 console.log("Cart script successfullly loaded!")
 
 var cart = [];
+var runningTotal = 0;
 var cartText = document.getElementById("cartText");
+var runningText = document.getElementById("runningTotal");
+var finalCartText = document.getElementById("finalCart");
+var finalTotalText = document.getElementById("finalRunningTotal");
 
 function getCart() { 
-    return cart;
+    return sessionStorage.getItem("Cart");
+    //return cart;
+    
+}
+
+function getTotal() {
+    return sessionStorage.getItem("Total");
     
 }
 
 function resetCart() { 
     cart = [];
     cartText.innerHTML = "Empty";
+    runningTotal = 0;
+    runningText.innerHTML = "$0";
     //updateCart();
     
 }
 
-function addCart(item) {   
-    cart[cart.length] = item + "\n";
+function addCart(item, price) {   
+    cart[cart.length] = item;
+    runningTotal += price;
     updateCart();
+    updateRunningTotal();
     
 }
 
 function updateCart() {
-    
+    sessionStorage.setItem("Cart",cart);
     console.log(getCart());
     cartText.innerHTML = getCart();
     
 }
+
+function updateRunningTotal() {
+    sessionStorage.setItem("Total",runningTotal);
+    console.log(runningTotal);
+    runningText.innerHTML = "$" + runningTotal;
+    
+}
+
+function changePage(pageName) {   
+    if (window.location !== pageName) {
+        window.location = pageName;
+    }
+    
+}
+
+function finalCart() {
+        console.log(getCart());
+        finalCartText.innerHTML = getCart();
+        console.log(getTotal());
+        finalTotalText.innerHTML = "$" + getTotal();
+    
+}
+
+finalCart();
