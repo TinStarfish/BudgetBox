6a1275e885d19625dbb0fc52de21ac30d74fb190
Sarah DeWitt <SDeWitt@student.neumont.edu>
2018-02-22 17:23:28 -0700

    css and user profile set up
    Had trouble working with the user pool. Getting help on it this weekend. CSS not yet done for the products and cart pages. Will finish that up tonight and this weekend.

---
+++ BudgetBox-master/profileScript.js
@@ -0,0 +1,11 @@
+function profileSubmit(){
+    var proUN = document.getElementById("Username").value;
+    var proPW = document.getElementById("Password").value;
+    var proEmail = document.getElementById("Email").value;
+    var proName = document.getElementById("Name").value;
+    var proPhone = document.getElementById("PhoneNumber").value;
+    var proAddress = document.getElementById("Address").value;
+    var proCard = document.getElementById("CardInfo").value;
+    
+    //save or update user
+};
